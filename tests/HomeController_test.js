b4727d38bebf8befe6fb22e34935e87db273ba7d
Andrew Pierce <andrewpierce3141@gmail.com>
2016-05-18 20:36:55 -0700

    cleaned up tests, and began implementing tests for the home controller

--- tests/HomeController_test.js
+++ tests/HomeController_test.js
@@ -3,18 +3,18 @@
 describe('HomeController Tests :', function() {
 
   // Only loading one module which is being tested
-  beforeEach(module('mostPopularListingsApp.home'));
+  beforeEach(module('progressionGenerator.home'));
 
   describe('Loading HomeController. It...', function(){
 
     // custom variables needed during the tests
     var scope;
     var controller;
-    
+
     // Get the controller
     beforeEach(inject(function($controller,$rootScope){
-      scope = $rootScope.$new(); // initializa child of global $rootScope
-      controller = $controller("HomeController", {$scope: scope}); // Controller expects $scope
+      scope = $rootScope.$new();
+      controller = $controller("HomeController", {$scope: scope});
 
     }));
 
@@ -23,8 +23,10 @@ describe('HomeController Tests :', function() {
       expect(controller).toBeDefined();
     }));
 
-    it('should have property next defined', inject(function() {
-      expect(controller.message).toEqual("Hello Home!");    
+    it('should initially generate a progression of length 3-5', inject(function() {
+      var progLength = controller.progression.length;
+      expect(progLength).toBeGreaterThan(2);
+      expect(progLength).toBeLessThan(6); 
     }));
   });
 
